9bba19c7352c1828f97b8098fc39a0c37c882ade
Victoria <kochetit@gmail.com>
2025-09-18 22:11:57 +0300

    feat: prepear for tests

--- converter.js
+++ converter.js
@@ -1,23 +1,33 @@
 "use strict";
 
-let answer = prompt("You want to convert to Celsium or Fahrenheit? Press C/F").trim().toLowerCase();
-
-let degree;
-
-if (answer === "f" ){
-    degree = +prompt("Please, enter degree in Celsium to convert: ");
-   alert(`It is ${toF(degree).toFixed(1)} °F`);
-} else if (answer === "c"){
-    degree = +prompt("Please, enter degree in Fahrenheit to convert: ");
-    alert(`It is ${toC(degree).toFixed(1)} °C`);
-} else {
-    alert("Invalid choice. Please enter C or F.");
-}
-
 function toF(c){
     return c * 9/5 + 32;
 }
 
 function toC(f){
     return (f - 32) * 5/9;
+}
+
+let answer = prompt("You want to convert to Celsium or Fahrenheit? Press C/F").trim().toLowerCase();
+
+if (answer){
+    answer = answer.trim().toLowerCase();
+
+    let degree;
+
+    if (answer === "f" ){
+        degree = +prompt("Please, enter degree in Celsium to convert: ");
+    alert(`It is ${toF(degree).toFixed(1)} °F`);
+    } else if (answer === "c"){
+        degree = +prompt("Please, enter degree in Fahrenheit to convert: ");
+        alert(`It is ${toC(degree).toFixed(1)} °C`);
+    } else {
+        alert("Invalid choice. Please enter C or F.");
+    }
+} else {
+    alert("Canceled or empty input.");
+}
+
+if (typeof module !== "undefined") {
+    module.exports = {toF, toC};
 }
